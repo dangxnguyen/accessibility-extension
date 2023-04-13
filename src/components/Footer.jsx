17d88ecdfde4c8b37f82726d906a6ce2b34925e1
Dang Nguyen <xuanhaidang.nguyen@gmail.com>
2023-04-12 23:03:58 -0500

    file uploaded

---
+++ src/components/Footer.jsx
@@ -0,0 +1,49 @@
+import styles from "../style";
+import { logo1 } from "../assets";
+import { footerLinks } from "../constants";
+
+const Footer = () => (
+  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+      <div className="flex-[1] flex flex-col justify-start mr-10">
+        <div className="cursor-pointer flex items-center">
+          <img src={logo1} alt="access" className="w-[80px] h-[80px]" />
+          <h1 className="text-gradient text-5xl">AccessExtend</h1>
+        </div>
+        <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
+          A new tool to improve your web's experience easy, pleasing and effective.
+        </p>
+      </div>
+
+      <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+        {footerLinks.map((footerlink) => (
+          <div key={footerlink.title} className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}>
+            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
+              {footerlink.title}
+            </h4>
+            <ul className="list-none mt-4">
+              {footerlink.links.map((link, index) => (
+                <li
+                  key={link.name}
+                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
+                    index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
+                  }`}
+                >
+                  {link.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+
+    <div className="w-full flex justify-center items-center pt-6 border-t-[1px] border-t-[#3F3E45]">
+      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
+        Copyright Ⓒ 2023. All Rights Reserved.
+      </p>
+    </div>
+  </section>
+);
+
+export default Footer;
